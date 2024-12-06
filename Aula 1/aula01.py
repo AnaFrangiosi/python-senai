4be5f16bbec9e8edb0582fef13d14f2d6f22bcf3
AnaFrangiosi <ana.frangiosii@gmail.com>
2024-12-06 11:13:38 -0300

    Introdução programação (print)

---
+++ Aula 1/aula01.py
@@ -0,0 +1,12 @@
+print("Olá, mundo")
+print("tudo bem")
+print("adorei a visita")
+print("hoje foi incrivel")
+print("adoro sair")
+print("quero jogar volei")
+print("vou sair hoje")
+print("vou passear amanha")
+print("que dia lindo")
+print("vou andar de carro")
+
+
